63d1ed8f7b166cb3d2e6c0cea79d6dbae8099aae
nitish sahni <sahni@berkeley.edu>
2020-05-07 22:15:47 +0530

    Mail functionality done

--- fyind/master/views.py
+++ fyind/master/views.py
@@ -19,6 +19,7 @@ class Form(ModelForm):
         self.helper = FormHelper()
         self.helper.form_show_labels = False
 
+
 def index(request):
     form = Form(request.POST or None)
     if request.method == 'POST':
@@ -28,13 +29,13 @@ def index(request):
             application.status = "IP"
             application.save()
             company = application.contact_name
-            emailTo = application.email
+            emailTo = [application.email]
             subject = 'Thank you for your time, ' + company
             message = render_to_string('emailTemplate.html', {
                 'name': application.contact_name,
                 'company': application.company_name,
             })
-            email = EmailMessage(subject, message, from_email='service@fyind.io', to=[emailTo])
+            email = EmailMessage(subject, message, from_email='service@fyind.io', to=emailTo, , 'nitin@kamadhenu.io')
             email.send()
             messages.success(request, 'Thank you for your filling out the form. We will be reaching out to you soon.')
             return redirect('index')
